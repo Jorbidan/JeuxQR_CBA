9700ca8707d81098f8a1bf8b2dd5a114c4cf7a66
ANTHONY\Anthony <Anthony.cote@outlook.com>
2021-04-22 14:25:11 -0400

    Ajout QR code Activity

---
+++ QRCode/app/src/main/java/com/example/qrcode/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.qrcode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    Button btnScanBarcode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+
+        btnScanBarcode = findViewById(R.id.btnScanBarcode);
+
+        btnScanBarcode.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.btnScanBarcode:
+                startActivity(new Intent(MainActivity.this, ScannedBarcodeActivity.class));
+                break;
+        }
+
+    }
+    
+}
